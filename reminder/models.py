152084b3bde1cc27147a5d99c89a79d43303a09a
Genius-Excel <geniusexcel9@gmail.com>
2024-08-05 15:12:17 +0100

    Model module containing Django table definitions.

---
+++ reminder/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class ClientAppointment(models.Model):
+    first_name = models.CharField(max_length=150, null=True)
+    last_name = models.CharField(max_length=150, null=True)
+    mobile_number = models.CharField(max_length=50)
+    email_address = models.CharField(max_length=150, null=True)
+    selected_property = models.TextField()
+    appointment_date = models.DateField()
+    appointment_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.first_name} --- {self.appointment_date}"
