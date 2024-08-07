33d05c0b77f37a7d020697db317231136422c196
Genius-Excel <geniusexcel9@gmail.com>
2024-08-07 15:16:29 +0100

    Custom SMS reminder function added to the utils module.

--- reminder/utils.py
+++ reminder/utils.py
@@ -1,5 +1,7 @@
 import mailtrap as mt
 from django.conf import settings
+from decouple import config 
+import vonage
 
 # Send email custom function.
 def custom_email_sender(email_address, subject, message, sender_name):
@@ -13,3 +15,24 @@ def custom_email_sender(email_address, subject, message, sender_name):
 
     client = mt.MailtrapClient(token=settings.SMTP_API_TOKEN)
     client.send(mail)
+
+
+
+VONAGE_API_KEY=config("VONAGE_API_KEY")
+VONAGE_API_SECRET=config("VONAGE_API_SECRET")
+
+def custom_sms_sender(sms_sender, sms_recipient, sms_message):
+    client = vonage.Client(key=VONAGE_API_KEY, secret=VONAGE_API_SECRET)
+
+    print("Sending SMS")
+    try:
+        response_data = client.sms.send_message({
+            "from": sms_sender,
+            "to": sms_recipient,
+            "text": sms_message,
+        })
+
+        print("SMS Sent success")
+        print(response_data)
+    except Exception as e:
+        print(f"Not sending because: {e}")
