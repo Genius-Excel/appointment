c5243da825c373a2a8733187770373ff8bd82965
Genius-Excel <geniusexcel9@gmail.com>
2024-08-08 18:24:11 +0100

    Serializers class for CradenMore created.

--- reminder/serializers.py
+++ reminder/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import ClientAppointment
+from .models import ClientAppointment, CradenMooreClients
 
 class ClientAppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClientAppointment
+        fields = "__all__"
+
+
+class CradenMooreClientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CradenMooreClients
         fields = "__all__"
