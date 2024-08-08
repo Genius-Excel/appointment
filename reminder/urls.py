18e3797be477c85962c26f7e3995ca5179226c24
Genius-Excel <geniusexcel9@gmail.com>
2024-08-08 18:25:57 +0100

    New API endpoint added to the url paths module.

--- reminder/urls.py
+++ reminder/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('api/create-appointment/', views.CreateClientAppointment.as_view(), name='create-appointment'),
+    path('api/craden-moore-client/', views.CreateCradenMooreClient.as_view(), name='craden-moore-client'),
+
 ]
