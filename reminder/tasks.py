c296cbea3a5399d71cb1b8a5cd6673a9ec56347d
Genius-Excel <geniusexcel9@gmail.com>
2024-08-09 14:07:11 +0100

    Email reminder task logic modified for sending email reminders 2 hours before appointment time.

--- reminder/tasks.py
+++ reminder/tasks.py
@@ -15,21 +15,21 @@ def send_email_reminder(self):
     current_time = now.astimezone(timezone.get_default_timezone())
 
     one_day_ahead = current_time + timedelta(days=1)
-    one_hour_ahead = current_time + timedelta(hours=1)
+    two_hour_ahead = current_time + timedelta(hours=2)
     ten_minutes_ahead = current_time + timedelta(minutes=10)
 
     # Find out appointments one day ahead
     appointments_one_day = ClientAppointment.objects.filter(
         appointment_date=one_day_ahead.date(),
-        appointment_time__hour=one_hour_ahead.time().hour,
+        appointment_time__hour=two_hour_ahead.time().hour,
         appointment_time__minute=one_day_ahead.time().minute
     )
 
     # Find out appointment 1 hour ahead
-    appointments_one_hour = ClientAppointment.objects.filter(
-        appointment_date=one_day_ahead.date(),
-        appointment_time__hour=one_hour_ahead.time().hour,
-        appointment_time__minute=one_hour_ahead.time().minute,
+    appointments_two_hour = ClientAppointment.objects.filter(
+        appointment_date=two_hour_ahead.date(),
+        appointment_time__hour=two_hour_ahead.time().hour,
+        appointment_time__minute=two_hour_ahead.time().minute,
 
     )
 
@@ -53,9 +53,9 @@ def send_email_reminder(self):
         custom_sms_sender("Tros Technologies", appointment.mobile_number, sms_message_content)
 
 
-    for appointment in appointments_one_hour:
+    for appointment in appointments_two_hour:
         email_subject = 'Reminder: Property inspection.'
-        email_message = f"Hello {appointment.first_name}, kindly note that your appointment is about to start in an hour, location:{appointment.selected_property_address} see you there!"
+        email_message = f"Hello {appointment.first_name}, kindly note that your appointment is about to start in 2 hours from now, location:{appointment.selected_property_address} see you there!"
         sender = 'Favour Idowu'
         email_address = appointment.email_address
         custom_email_sender(email_address, email_subject, email_message, sender)
@@ -64,13 +64,13 @@ def send_email_reminder(self):
         sms_message_content = f"Hello {appointment.first_name}, kindly note that your appointment is coming up on {appointment.appointment_date}, location:{appointment.selected_property_address} see you there!"
         custom_sms_sender("Tros Technologies", appointment.mobile_number, sms_message_content)
 
-    for appointment in appointments_ten_minutes:
-        email_subject = 'Reminder: Property inspection.'
-        email_message = f"Hello {appointment.first_name}, kindly note that your appointment is about to start in 10 mins, location:{appointment.selected_property_address} see you there!"
-        sender = 'Favour Idowu'
-        email_address = appointment.email_address
-        custom_email_sender(email_address, email_subject, email_message, sender)
+    # for appointment in appointments_ten_minutes:
+    #     email_subject = 'Reminder: Property inspection.'
+    #     email_message = f"Hello {appointment.first_name}, kindly note that your appointment is about to start in 10 mins, location:{appointment.selected_property_address} see you there!"
+    #     sender = 'Favour Idowu'
+    #     email_address = appointment.email_address
+    #     custom_email_sender(email_address, email_subject, email_message, sender)
 
-        # Send outbound SMS Reminder
-        sms_message_content = f"Hello {appointment.first_name}, kindly note that your appointment is about to start in 10 mins, location:{appointment.selected_property_address} see you there!"
-        custom_sms_sender("Tros Technologies", appointment.mobile_number, sms_message_content)
+    #     # Send outbound SMS Reminder
+    #     sms_message_content = f"Hello {appointment.first_name}, kindly note that your appointment is about to start in 10 mins, location:{appointment.selected_property_address} see you there!"
+    #     custom_sms_sender("Tros Technologies", appointment.mobile_number, sms_message_content)
