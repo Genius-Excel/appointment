9d2b43b93eb5fc834483e0ab994511e64f6ba0fa
Genius-Excel <geniusexcel9@gmail.com>
2024-08-12 19:20:01 +0100

    API view class for creating and sending Enish restaurant booking implemented.

--- reminder/views.py
+++ reminder/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import ClientAppointment, CradenMooreClients
-from .serializers import ClientAppointmentSerializer, CradenMooreClientsSerializer
+from .models import ClientAppointment, CradenMooreClients, EnishBookings
+from .serializers import ClientAppointmentSerializer, CradenMooreClientsSerializer, EnishBookingsSerializer
 from django.http import HttpResponse
 from .utils import custom_email_sender, custom_sms_sender
 
@@ -24,6 +24,26 @@ class CreateCradenMooreClient(generics.CreateAPIView):
         SMS_recipient = user.mobile_number
 
         custom_sms_sender(SMS_sender, SMS_recipient, SMS_meesage)
+
+
+class CreateEnishBooking(generics.CreateAPIView):
+    queryset = EnishBookings.objects.all()
+    serializer_class = EnishBookingsSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+
+        SMS_sender = "Enish"
+        SMS_message = f"""Hello {user.first_name},
+
+Your table reservation at {user.restaurant_location} for {user.number_of_guests} guests is confirmed for {user.appointment_date}.
+We look forward to welcoming you!"""
+        
+        SMS_recipient = user.mobile_number
+        custom_sms_sender(SMS_sender, SMS_recipient, SMS_message)
+        # custom_email_sender('', "Enish Testing", SMS_message, "Enish Restaurant")
+
+
        
     
 
@@ -31,4 +51,5 @@ class CreateCradenMooreClient(generics.CreateAPIView):
 
 def home(request):
     return HttpResponse("The number has been sumed")
+
 
