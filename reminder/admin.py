c88afbdf5573f61bbb005f16659c4c19bd8d4579
Genius-Excel <geniusexcel9@gmail.com>
2024-08-12 19:22:44 +0100

    'EnishBookings' model added to admin panel for record visiualisation.

--- reminder/admin.py
+++ reminder/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ClientAppointment, CradenMooreClients
+from .models import ClientAppointment, CradenMooreClients, EnishBookings
 
 # Register your models here.
 
 admin.site.register(ClientAppointment)
 admin.site.register(CradenMooreClients)
+admin.site.register(EnishBookings)
