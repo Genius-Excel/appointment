e1ad031d34d591ee5d4e9a629e81890291f8323f
Genius-Excel <geniusexcel9@gmail.com>
2024-08-12 19:20:50 +0100

    API endpoint for creating Enish booking added to list of url paths.

--- reminder/urls.py
+++ reminder/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('api/create-appointment/', views.CreateClientAppointment.as_view(), name='create-appointment'),
     path('api/craden-moore-client/', views.CreateCradenMooreClient.as_view(), name='craden-moore-client'),
+    path('api/enish-create-booking/', views.CreateEnishBooking.as_view(), name='enish-creating-booking'),
 
 ]
