20f85b34508b78b0e73fa8c567382934e4e26962
Genius-Excel <geniusexcel9@gmail.com>
2024-10-14 10:46:13 +0100

    New table added to te admin page for monitoring.

--- reminder/admin.py
+++ reminder/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ClientAppointment, CradenMooreClients, EnishBookings
+from .models import ClientAppointment, CradenMooreClients, EnishBookings, LaundryClinicCustomerQuery
 
 # Register your models here.
 
 admin.site.register(ClientAppointment)
 admin.site.register(CradenMooreClients)
 admin.site.register(EnishBookings)
+admin.site.register(LaundryClinicCustomerQuery)
