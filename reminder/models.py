4b57af81f66cf40a99ddb0352df346798e4df4c4
Genius-Excel <geniusexcel9@gmail.com>
2024-11-08 11:41:35 +0100

    'call_recordings' field changed to nullable.

--- reminder/models.py
+++ reminder/models.py
@@ -115,7 +115,7 @@ class LaundryClinicVoiceCall(models.Model):
     location = models.CharField(max_length=150, null=True)
     call_summary = models.TextField()
     call_transcript = models.TextField()
-    call_recordings = models.FileField(upload_to='laundry-clinic-ai-call-recordings/', null=True)
+    call_recordings = models.TextField(null=True)
 
 
 class CeraCerni(models.Model):
