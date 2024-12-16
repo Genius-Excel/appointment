ea733cf352bc17a18b8785651253c875b9df242d
Genius-Excel <geniusexcel9@gmail.com>
2024-12-16 13:16:27 +0100

    Forms model modified to override default attributes.

--- reminder/forms.py
+++ reminder/forms.py
@@ -7,3 +7,13 @@ class ImageAdsForm(forms.ModelForm):
     class Meta:
         model = ImageAds
         fields = ['image_url']
+
+        widgets = {
+            'image_url': forms.TextInput(attrs={'placeholder': 'Enter Image URL'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ImageAdsForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'id': 'imageUrl'})
